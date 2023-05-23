01abfaaf2202c48dd3ce768917246958b75f8a0d
NathBel <bellus.nathan@gmail.com>
2023-05-23 15:19:24 +0200

    :bug: fix bug deployement

--- db.js
+++ db.js
@@ -1,14 +1,22 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const url = require('url');
+
+const dbUrl = process.env.DATABASE_URL; // assuming this is where you have stored the DSN
+const params = url.parse(dbUrl);
+
+
 const connection = mysql.createConnection({
          connectionLimit: 10,
-         host     : process.env.HOST,
-         user     : process.env.USER,
-         password : process.env.PASSWORD,
-         database: process.env.database,
-         port: process.env.DB_PORT
+         host: params.hostname,
+         user: params.auth.split(':')[0],
+         password: params.auth.split(':')[1],
+         database: params.pathname.slice(1),
+         port: params.port,
+
+         
 });
 
 // open the MySQL connection
